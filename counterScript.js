e639a51a1eae2b4af4eabaa95f9bc0adb058ffbd
Vinay-Dakshanthaka <vinayd098@gmail.com>
2022-11-13 12:15:48 +0530

    increment, Decrement and reset

---
+++ counterScript.js
@@ -0,0 +1,21 @@
+
+let i = 0
+function increment(){
+      
+    i +=1;
+   document.getElementById("displayText").innerHTML =  i ;
+}
+
+function decrement(){
+      
+    i -=1;
+   document.getElementById("displayText").innerHTML =  i ;
+}
+
+
+function reset(){
+    i=0
+    document.getElementById("displayText").innerHTML =  i ;
+}
+
+
